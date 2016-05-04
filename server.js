2b20a5a5f4301e416f4ab4de1a47a96a1ed6a450
Alex <dualexua@gmail.com>
2016-05-04 11:24:01 +0300

    add db

--- server.js
+++ server.js
@@ -1,18 +1,40 @@
+/*inject modules*/
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require("request");
 var cheerio = require("cheerio");
+var MongoClient = require('mongodb').MongoClient;
 
+/*init ExApp*/
 var app = express();
 
-// parse application/x-www-form-urlencoded
+/*use parser*/
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-var url = 'http://fs.to/video/serials/i1JCtyIbttxmbahuYuAcA1i-flesh.html';
+var urlDB = 'mongodb://localhost:27017/test';
+var urlParse = 'http://fs.to/video/serials/i1JCtyIbttxmbahuYuAcA1i-flesh.html';
 
-request(url, function (error, response, body) {
+/*connect to DB*/
+MongoClient.connect(urlDB, function(err, db) {
+    if(err) throw err;
+    var dbCollection = db.collection('parser_collection');
+    // dbCollection.insert({name:'Andrew'}, function (err, docs) {
+    //     dbCollection.count(function (err, count) {
+    //         console.log('count', count);
+    //     });
+    //
+    //     dbCollection.find().toArray(function (err, results) {
+    //         console.log('results', results);
+    //         db.close();
+    //     });
+    // });
+    // console.log("Connected correctly to server.", db);
+});
+
+/*parse page*/
+request(urlParse, function (error, response, body) {
     if (!error) {
         var $ = cheerio.load(body);
         var lastWatchedMov;
@@ -29,12 +51,14 @@ app.get('/', function (req, res) {
     res.send('fff');
 });
 
+/*API*/
 app.post('/getlink/token=myapp', function (req, res) {
     console.log('req', req.body.name);
     res.send(req.body.name);
 
 });
 
+/*start web server on port 3000*/
 app.listen(3000, function () {});
 
 // $.ajax({url:'test/', type:'POST', data:'Blink182', success : function(response){console.log('response', response)}});
