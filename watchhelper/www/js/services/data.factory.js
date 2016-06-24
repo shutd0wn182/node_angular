164aaa258f245fc4b9e922c92b1793fdfaa3625f
shutD0wn182 <bwsua.kas@gmail.com>
2016-06-24 13:04:16 +0300

    remove deferred object $q

--- watchhelper/www/js/services/data.factory.js
+++ watchhelper/www/js/services/data.factory.js
@@ -1,40 +1,32 @@
-angular.module('watchHelper').factory('filmFactory', ['$http', '$q', function ($http, $q) {
+angular.module('watchHelper').factory('filmFactory', ['$http', function ($http) {
     // var addFilmUrl = 'http://evening-oasis-38864.herokuapp.com/addfilm/token=myapp';
     var addFilmUrl = 'http://localhost:5000/addfilm/token=myapp';
 
     return {
         getFilmData : function (_url) {
-            var deferred = $q.defer();
-
-            $http.post(_url).then(
+            return $http.post(_url).then(
                 function (response) {
-                    deferred.resolve(response.data);
+                    return response.data;
                 },
                 function (error) {
                     console.error('ERROR! Problem with connection to server ', error);
                 }
             );
-
-            return deferred.promise;
         },
-        
-        addFilm : function (_url, _email) {
-            var deferred = $q.defer();
 
-            $http.post(addFilmUrl,
+        addFilm : function (_url, _email) {
+            return $http.post(addFilmUrl,
                 {
                     filmUrl : _url,
                     userEmail : _email
                 }).then(
                 function (response) {
-                    deferred.resolve(response.data);
+                    return response.data;
                 },
                 function (error) {
                     console.error('ERROR! ', error);
                 }
             );
-
-            return deferred.promise;
         }
     }
 }]);
