07163981254cb9ca35bc7b572bf2a4abea28dbd3
shutD0wn182 <bwsua.kas@gmail.com>
2016-07-12 12:20:18 +0300

    change FE fonts, change SMTP config , add smtp module

--- tools.js
+++ tools.js
@@ -1,7 +1,16 @@
 var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
 
 var Tools = function(){
-    this.transporter = nodemailer.createTransport('smtps://masterrandrew%40gmail.com:Blink182182@smtp.gmail.com');
+    // this.transporter = nodemailer.createTransport('smtps://masterrandrew%40gmail.com:Blink182182@smtp.gmail.com');
+    this.transporter = nodemailer.createTransport(smtpTransport({
+        host : 'mail.smtp2go.com',
+        port : 2525,
+        auth : {
+            user : 'bwsua.kas@gmail.com',
+            pass : 'GKVfqkCBKOb8'
+        }
+    }));
 };
 
 Tools.prototype.sendMail = function (_user_email, _subject, _text, _html) {
